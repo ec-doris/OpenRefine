00ace1fc013141a11dda2c58b5e64be1653bbb60
Dennis Diefenbach <dennis.diefenbach@univ-st-etienne.fr>
2022-06-16 14:45:07 +0200

    Fixing bug https://github.com/OpenRefine/OpenRefine/issues/4931

--- extensions/wikidata/module/scripts/dialogs/manage-account-dialog.js
+++ extensions/wikidata/module/scripts/dialogs/manage-account-dialog.js
@@ -99,7 +99,7 @@ ManageAccountDialog.displayPasswordLogin = function (onSuccess) {
 
   // We don't support logging in with owner-only consumer if the target Wikibase doesn't support OAuth.
   if (!WikibaseManager.getSelectedWikibaseOAuth()) {
-    elmts.dialogFooter.hide();
+    //elmts.dialogFooter.hide();
   }
 
   var level = DialogSystem.showDialog(frame);
